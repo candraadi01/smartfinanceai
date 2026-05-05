2b13b851bf1f1d5562a34e2e06dbe0547d724441
saklni <sakhialni2005@gmail.com>
2026-05-05 21:12:47 +0700

    menambahkan fitur

--- src/layouts/DashboardLayout.jsx
+++
@@ -1,4 +0,0 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
-export default function DashboardLayout({ theme, onToggleTheme }){return <div className="app-shell"><Sidebar/><main className="content"><Topbar theme={theme} onToggleTheme={onToggleTheme}/><Outlet/></main></div>}
